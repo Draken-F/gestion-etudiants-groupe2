18ef2b15012b66080bf825caa0f751903497cb17
Florian706 <messangaflorian00@gmail.com>
2025-05-07 17:28:18 -0700

    Ajout du script de test pour la coherence des donnees

---
+++ scripts/test.php
@@ -0,0 +1,10 @@
+echo '<?php
+$xml = simplexml_load_file("../data/etudiants.xml");
+foreach ($xml->etudiant as $etudiant) {
+    if (empty($etudiant->id) || empty($etudiant->nom) || empty($etudiant->module)) {
+        echo "Erreur : Donnée manquante\n";
+    } else {
+        echo "ID: " . $etudiant->id . " est valide\n";
+    }
+}
+?>' > scripts/test.php
